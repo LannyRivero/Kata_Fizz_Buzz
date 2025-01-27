60b4bb91c29e02e358626c3df3b825b66aa78657
LannyRivero <lannyrivero288@gmail.com>
2025-01-27 17:09:35 +0100

    FizzBuzz complete

--- src/main/java/dev/lanny/kata_fizzbuzz/App.java
+++ src/main/java/dev/lanny/kata_fizzbuzz/App.java
@@ -1,17 +1,15 @@
 package dev.lanny.kata_fizzbuzz;
 
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FizzBuzz fizzbuzz = new FizzBuzz();
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzbuzz.checkNumber(i));
+        }
+        System.out.println("Fin del programa FizzBuzz!");
     }
 }
